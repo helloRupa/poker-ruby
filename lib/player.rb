3196a5f3b5b3d4ec79a24cf1d5eced75c421a421
Rupa Dhillon <rupa211984@gmail.com>
2019-03-01 00:31:08 -0800

    Get player input

--- lib/player.rb
+++ lib/player.rb
@@ -2,3 +2,87 @@
 # Player has methods to ask the user:
 #     Which cards they wish to discard
 #     Whether they wish to fold, see, or raise.
+
+class Player
+  attr_reader :name, :cards, :purse
+  attr_accessor :last_payment
+
+  def initialize(name, purse, cards)
+    @name = name
+    @purse = purse
+    @cards = cards
+    @last_payment = 0
+  end
+
+  def bank(amount)
+    @purse += amount
+  end
+
+  def input_cards_discard
+    puts 'Which cards would you like to replace (max. 3)? (e.g. 0, 2, 4)'
+    puts 'Or type none if you wish to keep all of your cards:'
+    print '> '
+    replace_choice
+  rescue ArgumentError
+    puts 'Numbers 0 - 4 only, 3 choices max, or none'
+    retry
+  end
+
+  def replace_cards(indexes, new_cards)
+    remove_cards(indexes)
+    add_cards(new_cards)
+  end
+
+  def input_call
+    puts 'Would you like to fold, see, or raise?'
+    print '> '
+    call_choice
+  rescue ArgumentError
+    puts 'Please select only fold, see, or raise.'
+    retry
+  end
+
+  def input_raise
+    puts 'How much would you like to raise by?'
+    print '> '
+    raise_answer
+  rescue ArgumentError
+    puts 'Please input numbers only and ensure you have enough money.'
+    retry
+  end
+
+  private
+
+  def raise_answer
+    answer = gets.chomp
+    raise ArgumentError if answer.match(/^\d+$/).nil? || answer.to_i > @purse
+    answer.to_i
+  end
+
+  def call_choice
+    answer = gets.chomp.downcase
+    raise ArgumentError unless %w[fold see raise].include?(answer)
+    answer
+  end
+
+  def replace_choice
+    choices = gets.chomp
+    choices.delete!(' ')
+    return 'none' if choices.downcase == 'none'
+    choice_arr = choices.split(',')
+    unless choice_arr.all? { |choice| choice.between?('0', '4') } && choice_arr.length == 3
+      raise ArgumentError
+    end
+    choice_arr.map(&:to_i)
+  end
+
+  def remove_cards(indexes)
+    cards_left = []
+    @cards.each_with_index { |card, idx| cards_left << card unless indexes.include?(idx) }
+    @cards = cards_left
+  end
+
+  def add_cards(cards)
+    @cards += cards
+  end
+end
